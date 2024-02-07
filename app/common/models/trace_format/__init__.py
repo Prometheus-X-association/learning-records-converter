c6a74a70847a95040fe725e94bcb52e0956c58a9
Jonathan <jonathan.renard2@gmail.com>
2024-02-07 08:45:39 +0000

    move trace formats models to isolate from rest of project

--- app/common/models/trace_format/__init__.py
+++
@@ -1,1 +0,0 @@
-from app.common.models.trace_format.scorm_1_1 import SCORMDataModel
---
+++ app/common/trace_formats/models/__init__.py
@@ -0,0 +1,2 @@
+from trace_formats.models.scorm_1_1 import SCORMDataModel
+from trace_formats.models.xapi.base.statements import BaseXapiStatement
