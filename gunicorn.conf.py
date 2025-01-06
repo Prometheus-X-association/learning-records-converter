1fc3de0df49968126a3e1467f4188f6334ecefe9
pierrocknroll <pierrock+github@protonmail.com>
2025-01-06 15:02:49 +0100

    :rotating_light: Lint fixes

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -10,9 +10,9 @@ bind = f"{os.getenv('APP_INTERNAL_HOST', '0.0.0.0')}:{os.getenv('APP_INTERNAL_PO
 reload = (os.getenv("ENVIRONMENT", "development") == "development")
 
 # Worker Processes
-workers = int(os.getenv('WORKERS_COUNT',  multiprocessing.cpu_count() * 2 + 1))
-threads = int(os.getenv("THREADS_PER_WORKER", 1))
-worker_class = 'uvicorn.workers.UvicornWorker'
+workers = int(os.getenv("WORKERS_COUNT",  multiprocessing.cpu_count() * 2 + 1))
+threads = int(os.getenv("THREADS_PER_WORKER", "1"))
+worker_class = "uvicorn.workers.UvicornWorker"
 
 # Logging
 loglevel = os.getenv("LOG_LEVEL", "info").lower()
